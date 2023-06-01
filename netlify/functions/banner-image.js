3424566816edd8191d751bc51c6399943f6db36c
Luis Quintana <plissken2013es@gmail.com>
2023-06-01 13:22:54 +0200

    Add title and text params & more content

--- netlify/functions/banner-image.js
+++ netlify/functions/banner-image.js
@@ -6,16 +6,34 @@ exports.handler = async (event, context) => {
 		"You're gonna need a bigger boat.",
 		"Go ahead, punk, make my day.",
 		"Fortune and glory, kiddo, fortune and glory...",
-		"I don't drink... wine."
+		"I don't drink... wine.",
+		"Here's looking at you, kid.",
+		"Houston, we have a problem.",
+		"There's no crying in baseball!",
+		"A martini. Shaken, not stirred.",
+		"You can't handle the truth!",
+		"Where we're going we don't need roads."
+	]
+
+	const cta = [
+		"Descúbrelo",
+		"Vamos a por ello",
+		"Ni lo pienses",
+		"¿Qué mas da?",
+		"¡Hola, caracola!",
+		"Miraquitivoidisí",
+		"Hey, ho, let's go!"
 	]
  
-	const randomQuote = quotes[Math.floor(Math.random() * quotes.length)]
+	const randomTitle = quotes[Math.floor(Math.random() * quotes.length)]
+	const randomText = cta[Math.floor(Math.random() * cta.length)]
 	const { width = "600" } = event.queryStringParameters;
 	const { height = "400" } = event.queryStringParameters;
 	const { targetURL = "https://www.create-store.com/es/content/1629-magazine-index" } = event.queryStringParameters
  
 	const response = JSON.stringify({ 
-		text: randomQuote, 
+		title: randomTitle,
+		text: randomText,
 		image_url: "http://placekitten.com/" + width + "/" + height,  
 		height: height,
 		width: width,
